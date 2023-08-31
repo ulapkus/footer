9c673ea9964fd90673812987684f4f11b3c0fc9c
ulapkus <ulapkus@gmail.com>
2023-08-30 21:59:27 -0400

    Imported Roboto Mono font and incorporated module css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
+
 import React from "react";
 import Head from "next/head";
+import styles from "./styles.module.css";
 
 function App() {
   return (
@@ -10,55 +13,55 @@ function App() {
       <footer>
         <main>
           <img
-            className="image"
+            className={styles.image}
             src="https://hngr-icons.s3.amazonaws.com/supperclub/ticketdex/xp_logo_4+1.png"
             alt="XP logo"
           ></img>
-          <section className="links-one">
-            <h1 className="xp-tickets">XP TICKETS</h1>
+          <section className={styles.linksOne}>
+            <h1>XP TICKETS</h1>
             <a href="www.google.com">About XP</a>
             <a href="www.google.com">Upcoming Events</a>
             <a href="www.google.com">Sell Your Tickets</a>
           </section>
-          <section className="links-two">
-            <section className="icon-group">
+          <section className={styles.linksTwo}>
+            <section className={styles.iconGroup}>
               <a href="www.google.com">
                 <img
-                  className="icon"
+                  className={styles.icon}
                   src="https://i.ibb.co/4FdMwHs/Twitter.png"
                   alt="Twitter logo"
                 ></img>
               </a>
               <a href="www.google.com">
                 <img
-                  className="icon"
+                  className={styles.icon}
                   src="https://i.ibb.co/hmJ1kLq/Discord.png"
                   alt="Discord logo"
                 ></img>
               </a>
               <a href="www.google.com">
                 <img
-                  className="icon"
+                  className={styles.icon}
                   src="https://i.ibb.co/RjbNQsL/Threads.png"
                   alt="Threads logo"
                 ></img>
               </a>
               <a href="www.google.com">
                 <img
-                  className="icon"
+                  className={styles.icon}
                   src="https://i.ibb.co/1sX8YPw/Mastodon.png"
                   alt="Mastodon logo"
                 ></img>
               </a>
             </section>
-            <section class="links-two-without-icons">
+            <section className={styles.linksTwoNoIcons}>
               <a href="www.google.com">Tamperproof</a>
               <a href="www.google.com">Captain Labs</a>
               <a href="www.google.com">Contact Us</a>
             </section>
           </section>
-          <div id="rightside-gap">
-            <section className="rightside">
+          <div className={styles.rightsideGap}>
+            <section className={styles.rightside}>
               <h1>STAY IN THE KNOW</h1>
               <input placeholder="Name" />
               <input type="email" placeholder="Email" />
@@ -66,7 +69,7 @@ function App() {
             </section>
           </div>
         </main>
-        <div className="xp-by-captain">
+        <div className={styles.xpByCaptain}>
           XP BY CAPTAIN / 2023 All Rights Reserved
         </div>
       </footer>
